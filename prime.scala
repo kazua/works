426124f0fc3815a1488c06ba0592e6f2cd3cda25
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-10-02 20:43:04 +0900

    jojo Eratosthenes' sieve
    jojo!

--- prime.scala
+++ prime.scala
@@ -3,15 +3,23 @@ object prime {
     def prime(sl: List[Int]): List[Int] =
         if (sl.head != 2 && (sl.isEmpty || Math.pow(plist.head,2)>sl.last)){
             plist = plist.reverse ::: sl
-            plist.foreach { println }
-            Nil
+            plist
         }else{
             plist = sl.head :: plist
             sl.head :: prime(sl.tail.filter(_ % sl.head != 0))
+            plist
         }
+
+    def jojo(pl: List[Int],sl: List[Int]){
+        for(i <- sl)
+            if(pl.exists(_ == i))
+                println("jojo!")
+            else
+                println(i)
+    }
 
     def main(args: Array[String]) {
         var i = 100
-        prime(Range(2, i).toList)
+        jojo(prime(Range(2, i).toList),Range(1, i).toList)
     }
 }
