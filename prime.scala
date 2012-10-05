16e8a8443c8976e5cafd4e02d895f34fca6df7b7
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-10-05 20:01:08 +0900

    さらに高速化
    もうこれで終わり。
    もっと速いやり方でやってる人は教えてください。勉強させてください。

--- prime.scala
+++ prime.scala
@@ -1,9 +1,8 @@
 object prime {
     var plist = List.empty[Int]
     def prime(sl: List[Int]): List[Int] =
-        if (sl.head != 2 && (sl.isEmpty || Math.pow(plist.head,2)>sl.last)){
-            plist = plist.reverse ::: sl
-            plist
+        if (sl.head != 2 && (sl.isEmpty || Math.pow(plist.head,2) > sl.last)){
+            plist.reverse ::: sl
         }else{
             plist = sl.head :: plist
             prime(sl.tail.filter(_ % sl.head != 0))
@@ -11,17 +10,20 @@ object prime {
 
     def jojo(pl: List[Int],sl: List[Int]){
         var pls = pl
-        for(i <- sl)
-            if(pls.exists(_ == i)){
+        var plst = 0
+        for(i <- sl){
+            if(!pls.isEmpty) plst = pls.head
+            if(plst == i){
                 pls = pls.filter{ _ != i }
                 println("jojo!")
             }else{
                 println(i)
             }
+        }
     }
 
     def main(args: Array[String]) {
         var i = 100
-        jojo(prime(Range(2, i+1).toList),Range(1, i+1).toList)
+        jojo(prime(Range(2, i + 1).toList),Range(1, i + 1).toList)
     }
 }
