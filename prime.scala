8245a014172615e4178e08eb6f236c05dcc95b26
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-10-06 10:45:39 +0900

    ちょっとコメント+

--- prime.scala
+++ prime.scala
@@ -23,7 +23,7 @@ object prime {
     }
 
     def main(args: Array[String]) {
-        var i = 500000
+        var i = 100//ここまでの数字内で処理
         jojo(prime(Range(2, i + 1).toList),Range(1, i + 1).toList)
     }
 }
