f9414d32c259b2ee5c3fd58eb75101e70e36ec0a
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-04 14:26:30 +0900

    パターンマッチングを使うように変更

--- qksort.scala
+++ qksort.scala
@@ -1,8 +1,10 @@
 object qksort {
-    def qksort(sl: List[Int]): List[Int] =
-        if (sl.length <= 1) sl
-        else List.concat(qksort(sl.filter(sl(sl.length / 2) >)), sl.filter(sl(sl.length / 2) ==), qksort(sl.filter(sl(sl.length / 2) <)))
-
+    def qksort(sl: List[Int]): List[Int] = {
+        sl match {
+            case List() => sl
+            case slh::sll => qksort(sll.filter(slh > _)) ::: List(slh) ::: qksort(sll.filter(slh < _))
+        }
+    }
     def main(args: Array[String]) {
         var ls = List(5, 27, 15, 31, 13, 23, 49, 16, 53, 39, 24, 11, 21, 63, 87)
         qksort(ls).foreach{println}
