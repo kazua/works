e85401721217855581ca722401e58f7a229054a6
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-14 20:14:23 +0900

    Project Euler Problem 21

---
+++ problem21.scala
@@ -0,0 +1,14 @@
+//http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2021
+//K.A
+object problem21 {
+    def main(args : Array[String]) {
+        var amicable = 0
+        Range(1,10000).toList.foreach{
+            num => {
+                val divisora = Range(1,num + 1).toList.filter(num % _ == 0).sum - num
+                if (Range(1, divisora + 1).toList.filter(divisora % _ == 0).sum - divisora == num && divisora != num) amicable += 1
+            }
+        }
+        println(amicable)
+    }
+}
