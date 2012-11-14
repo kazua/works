cbf824071dc03f9249f9ae244fe9e62037f1663c
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-15 02:15:27 +0900

    Project Euler Problem(mainから関数化)

--- problem20.scala
+++ problem20.scala
@@ -1,8 +1,13 @@
 //http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2020
+//K.A
 object problem20 {
-    def main(args : Array[String]) {
+    def problem20(is : Int) : Int ={
         var sum = 0
-        (BigInt(1) to 100).reverse.product.toString.split("").filter(_ != "").foreach(sum += _.toInt)
-        println(sum)
+        (BigInt(1) to is).reverse.product.toString.split("").filter(_ != "").foreach(sum += _.toInt)
+        sum
+    }
+    def main(args : Array[String]) {
+        val ploblem20k = problem20 _
+        println(ploblem20k(100))
     }
 }
--- problem21.scala
+++ problem21.scala
@@ -1,14 +1,18 @@
 //http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2021
 //K.A
 object problem21 {
-    def main(args : Array[String]) {
+    def problem21(is : Int) : Int = {
         var amicable = 0
-        Range(1,10000).toList.foreach{
+        Range(1,is).toList.foreach{
             num => {
                 val divisora = Range(1,num + 1).toList.filter(num % _ == 0).sum - num
                 if (Range(1, divisora + 1).toList.filter(divisora % _ == 0).sum - divisora == num && divisora != num) amicable += 1
             }
         }
-        println(amicable)
+        amicable
+    }
+    def main(args : Array[String]) {
+        val problem21k = problem21 _
+        println(problem21k(10000))
     }
 }
