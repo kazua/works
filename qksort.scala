b4e7b310c46b0fc3aef06a39e72ab637828ab4be
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-16 00:00:35 +0900

    クイックソート（パターンマッチ）（var→val修正）

--- qksort.scala
+++ qksort.scala
@@ -6,7 +6,7 @@ object qksort {
         }
     }
     def main(args: Array[String]) {
-        var ls = List(5, 27, 15, 31, 13, 23, 49, 16, 53, 39, 24, 11, 21, 63, 87)
+        val ls = List(5, 27, 15, 31, 13, 23, 49, 16, 53, 39, 24, 11, 21, 63, 87)
         qksort(ls).foreach{println}
     }
 }
