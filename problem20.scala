3520ab61d25f4f5b847050ac7cf41bd1e856d074
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-17 09:52:29 +0900

    Project Euler Problem 20(var→val対応)

--- problem20.scala
+++ problem20.scala
@@ -1,10 +1,13 @@
 //http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2020
 //K.A
+
 object problem20 {
+    def sum(xs : List[String]) : Int = xs match {
+        case Nil => 0
+        case y :: ys => y.toInt + sum(ys)
+    }
     def problem20(is : Int) : Int ={
-        var sum = 0
-        (BigInt(1) to is).reverse.product.toString.split("").filter(_ != "").foreach(sum += _.toInt)
-        sum
+        sum((BigInt(1) to is).reverse.product.toString.split("").filter(_ != "").toList)
     }
     def main(args : Array[String]) {
         val ploblem20k = problem20 _
