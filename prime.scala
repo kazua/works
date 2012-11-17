b06e6605008faa6c20819d9c6e6a2075ae8d487c
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-17 17:57:44 +0900

    素数の時だけjojo

--- prime.scala
+++ prime.scala
@@ -4,29 +4,29 @@ object prime {
     def prime(sl : List[Int], ms : Int) : List[Int] = sl match {
         case Nil => Nil
         case sl => {
-        if (sl.head != 2 && (sl.isEmpty || pow(ms, 2) > sl.last)) sl
-        else {
-            println(sl.head)
-            println(sl.tail.filter(_ % sl.head != 0))
-            sl.head :: prime(sl.tail.filter(_ % sl.head != 0),sl.head)
-        }
+            if (sl.head != 2 && (sl.isEmpty || pow(ms, 2) > sl.last)) sl
+            else {
+                println(sl.head)
+                println(sl.tail.filter(_ % sl.head != 0))
+                sl.head :: prime(sl.tail.filter(_ % sl.head != 0), sl.head)
+            }
         }
     }
 
-    def jojo(pl: List[Int],sl: List[Int]){
+    def jojo(pl : List[Int], sl : List[Int]) {
         var pls = pl
         var plst = 0
-        for(i <- sl){
-            if(!pls.isEmpty && plst != pls.head) plst = pls.head
-            if(plst == i){
+        for (i <- sl) {
+            if (!pls.isEmpty && plst != pls.head) plst = pls.head
+            if (plst == i) {
                 pls = pls.tail
                 println("jojo!")
-            }else println(i)
+            } else println(i)
         }
     }
 
-    def main(args: Array[String]) {
-        val i = 100//ここまでの数字内で処理
-        jojo(prime(Range(2, i + 1).toList,2),Range(1, i + 1).toList)
+    def main(args : Array[String]) {
+        val i = 100 //ここまでの数字内で処理
+        jojo(prime(Range(2, i + 1).toList, 2), Range(1, i + 1).toList)
     }
 }
