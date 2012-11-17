830a6abeadd01f389c63318f7f685f3b82132718
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-18 04:12:20 +0900

    Project Euler Problem 09

--- problem09.scala
+++ problem09.scala
@@ -7,7 +7,7 @@ object problem09 {
 
     def main(args : Array[String]) {
         val mc = 1000
-        val vc = for (b <- 2 until mc; a <- 1 until b; c = mc - a - b if pow(a, 2) + pow(b, 2) == pow(c, 2)) yield a * b * c
+        val vc = for (b <- 2 until mc; a <- 1 until b; c = mc - (a + b) if pow(a, 2) + pow(b, 2) == pow(c, 2)) yield a * b * c
 
         println(vc(0))
     }
