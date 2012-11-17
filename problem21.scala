70ebd72bc7154fc89f261b01db1f1f77935ffce5
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-17 10:40:21 +0900

    Project Euler Problem 21(var→val対応)
    スタックオーバーフロー回避・・・

--- problem21.scala
+++ problem21.scala
@@ -1,15 +1,17 @@
 //http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2021
 //K.A
+
 object problem21 {
-    def problem21(is : Int) : Int = {
-        var amicable = 0
-        Range(1,is).toList.foreach{
-            num => {
-                val divisora = Range(1,num + 1).toList.filter(num % _ == 0).sum - num
-                if (Range(1, divisora + 1).toList.filter(divisora % _ == 0).sum - divisora == num && divisora != num) amicable += 1
-            }
+    def calcAmicable(xs : List[Int]) : Int = xs match {
+        case Nil => 0
+        case y :: ys => {
+            val divisora = (Range(1,y + 1).toList.filter(y % _ == 0).sum - y)
+            (if (Range(1, divisora + 1).toList.filter(divisora % _ == 0).sum - divisora == y && divisora != y) 1 else 0) + calcAmicable(ys)
         }
-        amicable
+    }
+    def problem21(is : Int) : Int = {
+        val splLst = Range(1,is).toList.splitAt(is / 2)
+        calcAmicable(splLst._1) + calcAmicable(splLst._2)
     }
     def main(args : Array[String]) {
         val problem21k = problem21 _
