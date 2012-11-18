da0a38c1483341e3647787b46b9593dcad8256a7
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-18 13:49:32 +0900

    Project Euler Problem 07

--- problem07.scala
+++ problem07.scala
@@ -4,9 +4,13 @@
 import scala.math._
 
 object problem07 {
+    def getPrime(cnt : Int) : Int = {
+        val pr = 2 #:: Stream.from(3)
+        pr.filter(i=> pr.takeWhile(j=> pow(j, 2) <= i).forall(i % _ > 0))(cnt - 1)
+    }
     def main(args : Array[String]) {
         val cnt = 10001
-        val pr = 2 #:: Stream.from(3)
-        println(pr.filter(i=> pr.takeWhile(j=> pow(j, 2) <= i).forall(i % _ > 0))(cnt - 1))
+        val gpc = getPrime _
+        println(gpc(cnt))
     }
 }
