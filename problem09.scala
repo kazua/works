a8ef2a6ef4cdd00a9005ac5243060c60ec7ce2ae
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-18 10:56:22 +0900

    Project Euler Problem 09

--- problem09.scala
+++ problem09.scala
@@ -4,11 +4,14 @@
 import scala.math._
 
 object problem09 {
-
+    def pythagoras(mc : Int) : Int = {
+        val pg = for (b <- 2 until mc; a <- 1 until b; c = mc - (a + b) if pow(a, 2) + pow(b, 2) == pow(c, 2)) yield a * b * c
+        pg.sum
+    }
     def main(args : Array[String]) {
         val mc = 1000
-        val vc = for (b <- 2 until mc; a <- 1 until b; c = mc - (a + b) if pow(a, 2) + pow(b, 2) == pow(c, 2)) yield a * b * c
+        val pgk = pythagoras _
 
-        println(vc(0))
+        println(pgk(mc))
     }
 }
