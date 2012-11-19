e59b4ca82260e50c119102ccae1dbf055a69adea
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-19 15:37:12 +0900

    Project Euler Problem 21
    末尾再帰化

--- problem21.scala
+++ problem21.scala
@@ -2,25 +2,15 @@
 //K.A
 
 object problem21 {
-    def calcAmicable(xs : List[Int]) : Int = xs match {
-        case Nil => 0
+    def calcAmicable(acl : Int, xs : List[Int]) : Int = xs match {
+        case Nil => acl
         case y :: ys => {
             val divisora = (Range(1,y + 1).toList.filter(y % _ == 0).sum - y)
-            (if (Range(1, divisora + 1).toList.filter(divisora % _ == 0).sum - divisora == y && divisora != y) 1 else 0) + calcAmicable(ys)
-        }
-    }
-    def callCalc(xs : List[Int]) : Int = xs match {
-        case Nil => 0
-        case xs => {
-            if (xs.size <= 5000){
-                calcAmicable(xs)
-            }else{
-                calcAmicable(xs.take(5000)) + callCalc(xs.drop(5000))
-            }
+            calcAmicable(acl + (if (Range(1, divisora + 1).toList.filter(divisora % _ == 0).sum - divisora == y && divisora != y) 1 else 0), ys)
         }
     }
     def problem21(is : Int) : Int = {
-        callCalc(Range(1,is).toList)
+        calcAmicable(0, Range(1,is).toList)
     }
     def main(args : Array[String]) {
         val problem21k = problem21 _
