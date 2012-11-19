3905fba8ebe8f89e5f3c6b54fdd9e4e4d9025abc
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-19 15:49:51 +0900

    Project Euler Problem 22
    末尾再帰化

--- problem22.scala
+++ problem22.scala
@@ -5,14 +5,14 @@ import java.io._
 import scala.io.Source
 
 object problem22 {
-    def sum(s : String) : Int = s match {
-        case null => 0
-        case "" => 0
-        case s => (('a' to 'z').mkString.indexOf(s.charAt(0).toLower) + 1) + sum(s.substring(1))
+    def sum(acl : Int, s : String) : Int = s match {
+        case null => acl
+        case "" => acl
+        case s => sum(acl + (('a' to 'z').mkString.indexOf(s.charAt(0).toLower) + 1), s.substring(1))
     }
-    def score(ls : List[(String, Int)]) : Int = ls match {
-        case Nil => 0
-        case x::xs => ((x._2 + 1) * sum(x._1)) + score(xs)
+    def score(acl : Int, ls : List[(String, Int)]) : Int = ls match {
+        case Nil => acl
+        case x::xs => score(acl + ((x._2 + 1) * sum(0, x._1)), xs)
     }
     def problem22(path : String) {
         try {
@@ -20,7 +20,7 @@ object problem22 {
                 case d if d.isDirectory() => println("%s:%s".format(path, "指定パスはディレクトリです"))
                 case f if f.isFile() && f.canRead() && f.getName() == "names.txt" => {
                     try {
-                        val scp = score(Source.fromFile(f.getPath).getLines.toList.mkString.replace("\"", "").split(",").toList.sortWith(_.compareToIgnoreCase(_) < 0).zipWithIndex)
+                        val scp = score(0, Source.fromFile(f.getPath).getLines.toList.mkString.replace("\"", "").split(",").toList.sortWith(_.compareToIgnoreCase(_) < 0).zipWithIndex)
                         println(scp)
                     } catch {
                         case ex : java.nio.charset.MalformedInputException => println("%s:%s".format(f.getPath(), "検索対象外ファイルです"))
