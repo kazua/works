472c10a3496b4448fd2f6c4ec7e6530ba2d0d8ab
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-21 19:38:04 +0900

    おみくじ（確率調整）

--- writtenoracle.scala
+++ writtenoracle.scala
@@ -2,25 +2,24 @@ import scala.math._
 
 object writtenOracle {
 
-    def destiny(d : Double)(r : Double) : Int = floor(d * r).toInt
-
-    val destinyp = destiny(12)_
+    def intCalcD(d : Double)(r : Double) : Int = floor((d + 1) * r).toInt
+    val destinyp = intCalcD(_:Double)(random)
 
     def writtenOracle(name : String) : String = {
-        val destinypoint = destinyp(random)
+        val destinypoint = destinyp(100)
         destinypoint match {
-            case 11 => name + "の運勢は大凶です。"
-            case 10 => name + "の運勢は末凶です。"
-            case 9 => name + "の運勢は半凶です。"
-            case 8 => name + "の運勢は小凶です。"
-            case 7 => name + "の運勢は凶です。"
-            case 6 => name + "の運勢は末小吉です。"
-            case 5 => name + "の運勢は末吉です。"
-            case 4 => name + "の運勢は半吉です。"
-            case 3 => name + "の運勢は吉です。"
-            case 2 => name + "の運勢は小吉です。"
-            case 1 => name + "の運勢は中吉です。"
-            case 0 => name + "の運勢は大吉です。"
+            case 0 => name + "の運勢は大凶です。"
+            case i if (i > 0 && i <= 3) => name + "の運勢は末凶です。"
+            case i if (i > 3 && i <= 5) => name + "の運勢は半凶です。"
+            case i if (i > 5 && i <= 10) => name + "の運勢は小凶です。"
+            case i if (i > 10 && i <= 15) => name + "の運勢は凶です。"
+            case i if (i > 15 && i <= 25) => name + "の運勢は末小吉です。"
+            case i if (i > 25 && i <= 35) => name + "の運勢は末吉です。"
+            case i if (i > 35 && i <= 50) => name + "の運勢は半吉です。"
+            case i if (i > 50 && i <= 70) => name + "の運勢は吉です。"
+            case i if (i > 70 && i <= 80) => name + "の運勢は小吉です。"
+            case i if (i > 80 && i <= 95) => name + "の運勢は中吉です。"
+            case i if (i > 95 && i <= 100) => name + "の運勢は大吉です。"
         }
     }
 
