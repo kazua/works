e23a35e8be2fef080b1e9daed2f6783e0166cb2f
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-25 05:23:53 +0900

    Project Euler Problem 19

--- problem19.scala
+++ problem19.scala
@@ -4,12 +4,12 @@
 object problem19 {
     def getSunCnt : Int = {
         val dayCnt = List(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
-        val mlf = for(y <- 1900 to 1900; m <- 1 to 12) yield if(m == 2) if (if ((y % 100 == 0 && y % 400 != 0) || (y % 4 != 0)) false else true) 29 else 28
-                                                            else dayCnt(m - 1)
-        val ml = for(y <- 1901 to 2000; m <- 1 to 12) yield if(m == 2) if (if ((y % 100 == 0 && y % 400 != 0) || (y % 4 != 0)) false else true) 29 else 28
-                                                            else dayCnt(m - 1)
+        def mlf(st : Int, ed : Int) = for(y <- st to ed; m <- 1 to 12) yield if(m == 2) if (if ((y % 100 == 0 && y % 400 != 0) || (y % 4 != 0)) false else true) 29 else 28 else dayCnt(m - 1)
 
-        val mf = ml.foldLeft(List((1 + mlf.sum) % 7))((wf, d) => ((wf.head + d) % 7) :: wf)
+        val ml1900 = mlf(1900,1900)
+        val ml = mlf(1901,2000)
+
+        val mf = ml.foldLeft(List((1 + ml1900.sum) % 7))((wf, d) => ((wf.head + d) % 7) :: wf)
         mf.count(_ == 0)
     }
     def main(args : Array[String]) {
