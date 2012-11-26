f6a39d27ae46d9f7b3e398760b5315e4b288e242
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-26 22:06:46 +0900

    Project Euler Problem 02

--- problem02.scala
+++ problem02.scala
@@ -3,7 +3,7 @@
 
 object problem02 {
     def fbnEvnSum(mn : Int) : BigInt = {
-        lazy val fbn: Stream[Int] = 0 #:: fbn.scanLeft(1)((a, b) => a + b).takeWhile(_ <= mn)
+        lazy val fbn: Stream[Int] = 0 #:: fbn.scanLeft(1)(_+_).takeWhile(_ <= mn)
         fbn.filter(_ % 2 == 0).sum
     }
     def main(args : Array[String]) {
