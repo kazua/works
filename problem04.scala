923a9186346cd3f0aad5bae1e688ddb23c47572e
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-28 00:14:42 +0900

    Project Euler Problem 04

--- problem04.scala
+++ problem04.scala
@@ -7,7 +7,7 @@ object problem04 {
     def getPldNum(dig : Int) : Int = {
         val minNum : Int = 1 * pow(10,(dig - 1)).toInt
         val maxNum : Int = (1 * pow(10, dig)).toInt - 1
-        (minNum to maxNum).flatMap(i => (minNum to maxNum).map(_ * i)).filter(n => n.toString == n.toString.reverse).max
+        (minNum to maxNum).flatMap(i => (i to maxNum).map(_ * i)).filter(n => n.toString == n.toString.reverse).max
     }
     def main(args : Array[String]) {
         val dig = 3
