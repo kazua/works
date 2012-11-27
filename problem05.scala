bd1e4f6b78f97c736109133b2c545fe7b27a911d
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-11-28 00:59:42 +0900

    Project Euler Problem 05

---
+++ problem05.scala
@@ -0,0 +1,14 @@
+//http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%205
+//K.A
+
+import scala.math._
+
+object problem05 {
+    def getDivMin(sl : List[Int]) : Int = {
+        Range(sl.last, Int.MaxValue).takeWhile(i=> !sl.forall(i % _ == 0)).max + 1
+    }
+    def main(args : Array[String]) {
+        val sl = (1 to 20).toList
+        println(getDivMin(sl))
+    }
+}
