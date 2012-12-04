3c7f771e96fb7deecfa90028d974d15a36d06afa
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-12-04 21:45:18 +0900

    Project Euler Problem 12

---
+++ problem12.scala
@@ -0,0 +1,17 @@
+//http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2012
+//K.A
+
+import scala.math._
+
+object problem12 {
+    def getTrgFst(mn : Int) : Int = {
+        lazy val tn: Stream[Int] = 1 #:: Stream.from(2).scanLeft(1)((l, r) => l + r)
+        def divCnt(tn: Int) = (0 to Int.MaxValue -1).takeWhile(i => pow(i, 2) <= tn).reduceLeft((l, r) => if(tn % r == 0) l + 2 else l)
+
+        tn.find(divCnt(_) >= 500).min
+    }
+    def main(args : Array[String]) {
+        val mn = 500
+        println(getTrgFst(mn))
+    }
+}
