2c0264d062636c3a051c5a9a019e98b83ef28225
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-12-22 18:53:26 +0900

    Project Euler Problem 40

--- problem40.scala
+++ problem40.scala
@@ -1,16 +1,20 @@
 //http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2040
 //K.A
-
+ 
 object problem40 {
-    def getIrtlNum(cnt : Int): Int = {
-        val irtNum = Stream.from(1).takeWhile(_ <= cnt+1).mkString.map(_.toString.toInt)
-        irtNum(cnt).toInt
-    }
-    def getIrtlNumMlp(max : Int,acl : Int): Int = max match {
+    def getIrtlNumMlp(max: Int, irtNum: Seq[Int], acl: Int): Int = max match {
         case n if n < 1 => acl
-        case n => getIrtlNumMlp(max/10,acl*getIrtlNum(n-1))
+        case n => getIrtlNumMlp(max / 10, irtNum, acl * irtNum(n - 1))
     }
-    def main(args : Array[String]) {
-        println(getIrtlNumMlp(1000000,1))
+    def getMaxdgt(numdgt: Int, maxNumdgt: Int, maxNum: Int, acl: BigInt): BigInt = acl match {
+        case n if n >= maxNum => (BigInt(10).pow(numdgt - 1)-1) - ((n - maxNum)/(numdgt - 1))
+        case n => getMaxdgt(numdgt + 1, maxNumdgt, maxNum, n + (numdgt * (9 * BigInt(10).pow(numdgt - 1))))
+    }
+    def main(args: Array[String]) {
+        val maxMpl = 1000000
+        val maxNumdgt = maxMpl.toString.size
+        val maxDgt = getMaxdgt(1, maxNumdgt, maxMpl, 0)
+        val irtNum = Stream.from(1).takeWhile(_ <= maxDgt).mkString.map(_.toString.toInt)
+        println(getIrtlNumMlp(maxMpl, irtNum, 1))
     }
 }
