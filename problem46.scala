2b2acb0c4a611e79b357b106f07d1c8d55088d70
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-12-28 07:47:07 +0900

    Project Euler Problem 46

--- problem46.scala
+++ problem46.scala
@@ -4,11 +4,14 @@
 import scala.math._
 
 object problem46 {
-    def getOddCmpsNumMin() : BigInt = {
+    def getOddCmpsNumMin() : String = {
         lazy val pr : Stream[Int] = 2 #:: Stream.from(3).filter(i => pr.takeWhile(j => BigInt(j).pow(2) <= i).forall(i % _ > 0))
         lazy val or : Stream[Int] = Stream.from(2).filter(_ % 2 != 0).filter(i => pr.takeWhile(_ <= i).forall(_ != i))
 
-        or.dropWhile(i => pr.takeWhile(_ <= i).map(j => sqrt((i - j)/2)).forall(k => ceil(k) != floor(k)) == false).take(1).min
+        if (or.takeWhile(_ <= Int.MaxValue).dropWhile(i => pr.takeWhile(_ <= i).map(j => sqrt((i - j)/2)).forall(k => ceil(k) != floor(k)) == false) != Stream())
+            or.takeWhile(_ <= Int.MaxValue).dropWhile(i => pr.takeWhile(_ <= i).map(j => sqrt((i - j)/2)).forall(k => ceil(k) != floor(k)) == false)(0).toString
+        else
+            "Christian Goldbachの予想はInt型の範囲では正しい"
     }
     def main(args : Array[String]) {
         println(getOddCmpsNumMin())
