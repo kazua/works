dc665770bbd5f876da485804bc9e43a5809b295e
kazuhiro aida <k-aida@ah.em-net.ne.jp>
2012-12-28 20:03:34 +0900

    Project Euler Problem 46

--- problem46.scala
+++ problem46.scala
@@ -7,7 +7,7 @@ object problem46 {
     def getOddCmpsNumMin() : Int = {
         lazy val pr : Stream[Int] = 2 #:: Stream.from(3).filter(i => pr.takeWhile(j => BigInt(j).pow(2) <= i).forall(i % _ > 0))
         val or : Stream[Int] = Stream.from(2).filter(_ % 2 != 0).filter(i => pr.takeWhile(_ <= i).forall(_ != i))
-        val ormin : Stream[Int] = or.takeWhile(_ <= Int.MaxValue).dropWhile(i => pr.takeWhile(_ <= i).map(j => sqrt((i - j)/2)).forall(k => ceil(k) != floor(k)) == false)
+        val ormin : Stream[Int] = or.dropWhile(i => pr.takeWhile(_ <= i).map(j => sqrt((i - j)/2)).forall(k => ceil(k) != floor(k)) == false)
 
         ormin.take(1).min
     }
