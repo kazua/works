0527335f59e766eb6325bc007025694d76affc4a
kazua <loveisover777@gmail.com>
2013-01-02 22:10:07 +0900

    yeardiff

--- yeardiff.scala
+++ yeardiff.scala
@@ -2,10 +2,10 @@
 //年の中に同じ数値が現れない年のリスト
 
 object yeardiff {
-    def yeardiff(maxyear: Int): Seq[String] = {
-        (1 to maxyear).map(_.toString.toArray).filter(a => a.distinct.size == a.size).map(_.mkString)
+    def yeardiff(maxyear : Int) : Seq[String] = {
+        (1 to maxyear).map(_.toString.toList).filter(a => a.distinct.size == a.size).map(_.mkString)
     }
-    def main(args: Array[String]) {
+    def main(args : Array[String]) {
         val nowyear = 2013
         yeardiff(nowyear).foreach(println)
     }
