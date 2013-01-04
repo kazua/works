2ef1be8197a8b06875edab5b4ea1f5c0a35bb6fd
kazua <loveisover777@gmail.com>
2013-01-04 18:18:45 +0900

    Project Euler Problem 56

--- problem56.scala
+++ problem56.scala
@@ -3,7 +3,7 @@
 
 object problem56 {
     def getDigSumMax(mc : Int) : Int = {
-        (1 until mc).map(i => (1 to mc).map(j => (BigInt(i).pow(j)).toString.toList.map(_.toString.toInt).sum)).reduceLeft(_ ++ _).max
+        (1 until mc).map(i => (1 until mc).map(j => (BigInt(i).pow(j)).toString.toList.map(_.toString.toInt).sum)).reduceLeft(_ ++ _).max
     }
     def main(args : Array[String]) {
         val mCnt = 100
