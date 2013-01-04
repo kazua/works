b7f2ebaaac444b07e04bff16fcbd2d2b3e77eaa2
kazua <loveisover777@gmail.com>
2013-01-04 18:10:28 +0900

    Project Euler Problem 56

---
+++ problem56.scala
@@ -0,0 +1,12 @@
+//http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2056
+//K.A
+
+object problem56 {
+    def getDigSumMax(mc : Int) : Int = {
+        (1 until mc).map(i => (1 to mc).map(j => (BigInt(i).pow(j)).toString.toList.map(_.toString.toInt).sum)).reduceLeft(_ ++ _).max
+    }
+    def main(args : Array[String]) {
+        val mCnt = 100
+        println(getDigSumMax(mCnt))
+    }
+}
