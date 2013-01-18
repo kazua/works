65002006903dedd6f31d0852018152eff84f609e
kazua <loveisover777@gmail.com>
2013-01-18 21:27:58 +0900

    Project Euler Problem 36

---
+++ problem36.scala
@@ -0,0 +1,9 @@
+//http://odz.sakura.ne.jp/projecteuler/index.php?cmd=read&page=Problem%2036
+//K.A
+
+object problem36 {
+    def problem36 = (1 to 1000000).filter(i => i.toString == i.toString.reverse && i.toBinaryString == i.toBinaryString.reverse).sum
+    def main(args : Array[String]) {
+        println(problem36)
+    }
+}
