7e136f6bcef95be29e2330f21250918171014b84
kazua <loveisover777@gmail.com>
2013-11-02 03:47:05 +0900

    Project Euler Problem 21

--- problem21.scala
+++ problem21.scala
@@ -6,7 +6,7 @@ object problem21 {
         case Nil => acl
         case y :: ys => {
             val divisora = (Range(1,y + 1).toList.filter(y % _ == 0).sum - y)
-            calcAmicable(acl + (if (Range(1, divisora + 1).toList.filter(divisora % _ == 0).sum - divisora == y && divisora != y) 1 else 0), ys)
+            calcAmicable(acl + (if (Range(1, divisora + 1).toList.filter(divisora % _ == 0).sum - divisora == y && divisora != y) divisora else 0), ys)
         }
     }
     def problem21(is : Int) : Int = {
